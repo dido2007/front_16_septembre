1e07c3be22f0bb44818c53231427f070d798e5e5
Hedi Fourati <127599179+dido2007@users.noreply.github.com>
2023-09-16 13:05:34 +0100

    Add files via upload

---
+++ app/chat/page.jsx
@@ -0,0 +1,17 @@
+
+const MyProfile = () => {
+
+  return ( 
+    <>
+
+          <section className='flex items-center justify-center h-screen'>
+            <h1 className='text-3xl font-bold text-center'>
+              Aucun chat trouvé
+            </h1>
+          </section>
+
+    </>
+  );
+};
+
+export default MyProfile;
